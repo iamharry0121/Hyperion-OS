13618267f91501deca9fc388014572c190948f6e
Harish <harinavanee2011@gmail.com>
2026-06-10 21:55:09 -0400

    Made the minimize and close button work

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ let startY;
 let currentX;
 let currentY;
 let activeWindow;
+let osWindow;
 
 
 function toggleWindow() {
-    let osWindow = document.querySelector('.os-window'); //finds first html element w/ that name and selects it
+    osWindow = document.querySelector('.os-window'); //finds first html element w/ that name and selects it
 
     // Check if it's empty OR if it's set to 'none'
     if (osWindow.style.display === '' || osWindow.style.display === 'none') {
@@ -17,6 +18,10 @@ function toggleWindow() {
 }
 
 function dragStart(e) {
+    if (e.target.tagName === 'BUTTON') {
+            return; 
+        }
+
     activeWindow = e.target.closest('.os-window');
     startX = e.clientX //gets starting x
     startY = e.clientY //gets starting y
@@ -25,16 +30,16 @@ function dragStart(e) {
 }
 
 function dragMove(e) {
-    dx = startX-e.clientX //calcs distance between starting and where mouse is on x axis
-    dy = startY-e.clientY //calcs distance between starting and where mouse is on y axis
+    let dx = startX-e.clientX //calcs distance between starting and where mouse is on x axis
+    let dy = startY-e.clientY //calcs distance between starting and where mouse is on y axis
     startX = e.clientX //updates the new startx AFTER the change above
     startY = e.clientY //updates the new starty AFTER the change above
     
     if (activeWindow){
         //shifts the window horizontally by subtracting the mouse's X movement and adding 'px'
-        osWindow.style.left = (osWindow.offsetLeft - dx) + 'px';
+        activeWindow.style.left = (activeWindow.offsetLeft - dx) + 'px';
         //shifts the window vertically by subtracting the mouse's Y movement and adding 'px'
-        osWindow.style.top = (osWindow.offsetTop - dy) + 'px';
+        activeWindow.style.top = (activeWindow.offsetTop - dy) + 'px';
     }
 }
 
@@ -43,4 +48,16 @@ function dragEnd() {
     document.removeEventListener('mousemove', dragMove); 
     //stops listening for the mouse up after the user places the window down
     document.removeEventListener('mouseup', dragEnd);
+    activeWindow = null;
+}
+
+function xOut() {
+    osWindow = document.querySelector('.os-window');
+    if (osWindow) {
+        osWindow.style.display = 'none';
+        
+        // True Close: optional reset to center layout position
+        osWindow.style.left = '10%';
+        osWindow.style.top = '10%';
+    }
 }
